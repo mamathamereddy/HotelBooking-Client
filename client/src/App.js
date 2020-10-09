db7aa45cd4ca90b0834b9daf3f5553fe0343a9cb
mamathamereddy <mamathavangala1@gmail.com>
2020-10-09 14:00:00 +0100

    added images and edited app.js and index

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./containers /Home";
+import Rooms from "./containers /Rooms";
+import SingleRoom from "./containers /SingleRoom";
+import Error from "./containers /Error";
+
+import Navbar from "./components /Navbar";
+
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </>
   );
 }
 
-export default App;
+export default App
